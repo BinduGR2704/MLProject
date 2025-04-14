132071663987dbdda6b787376acd6f3bb466acaa
Bindu <bindugr2004@gmail.com>
2025-04-14 19:46:10 +0530

    Data Ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-from logger import logging
+from src.logger import logging
 
 def error_message_detail(error, error_detail:sys):
     _,_,exc_tb=error_detail.exc_info() # three inputs are taken in which the first 2 are neglected and 3rd one is exc_tb
